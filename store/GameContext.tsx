c98374b0ef14cc5272ead8c12fe268360b03dea5
han8129 <110092300+han8129@users.noreply.github.com>
2024-04-17 02:00:44 +0700

    Realize React State problem

--- store/GameContext.tsx
+++ store/GameContext.tsx
@@ -7,94 +7,34 @@ export const GameContext = createContext({
   happy: 100,
   events: Array<Event>,
   activities: Array<Activity>,
-    days: 1,
+  days: 1,
+    setHealth: () => {},
+    setDays: (days: number) => {}
 });
 
 const INTERVAL = 200;
 
 export default function GameContextProvider({ children }) {
-  const health = useRef(100);
+  const [health, setHealth] = useState(100);
   const money = useRef(10000);
   const happy = useRef(100);
   const events = useRef(new Array<Event>());
   const activities = useRef(new Array<Activity>());
   const loop = useRef(setInterval(() => {}, INTERVAL));
   const [days, setDays] = useState(100);
-  const monthCount = useRef(0);
-
-  useEffect(() => startLoop(), []);
+  const [monthCount, setMonthCount] = useState(0);
 
   const context = {
-    health: health.current,
+    health: health,
     money: money,
     happy: happy,
     events: events.current,
     activities: activities.current,
-      days: days
-    // setHealth: setHealth,
+    days: days,
+    setHealth: setHealth,
+      setDays: setDays,
+      monthCount: monthCount
   };
-
-  function startLoop() {
-    clearInterval(loop.current);
-
-    loop.current = setInterval(() => {
-        console.log(days);
-        setDays((current) => current + 1);
-      monthCount.current += 1;
-      if (money.current <= 0) {
-        health.current -= MAX_HEALTH * 0.05;
-      }
-
-      events.current = events.current.filter(
-        (e) => days < e.startDate + e.duration,
-      );
-
-      events.current.forEach((e: Event) => {
-        health.current += e.effect.health;
-        money.current += e.effect.money;
-        happy.current += e.effect.happy;
-      });
-
-      if (health.current <= 0) {
-        clearInterval(loop.current);
-          alert('You have died')
-          return;
-      }
-
-      // a random number in range [0, 9]
-      // 20% for event
-      if (monthCount.current > 29) {
-        monthCount.current = 0;
-        if ([0].includes(Math.floor(Math.random() * 3))) {
-          clearInterval(loop.current);
-          const randIndex = Math.floor(Math.random() * ALL_EVENTS.length);
-          const event = ALL_EVENTS[randIndex];
-          Alert.alert(`${event.name} happened`, "What will you do?", [
-            {
-              text: "A",
-              onPress: () => {
-                health.current += event.effect.health;
-                console.log("Pick A");
-                startLoop();
-              },
-            },
-            {
-              text: "B",
-              onPress: () => {
-                money.current += event.effect.money;
-                happy.current += event.effect.happy;
-                console.log("Pick B");
-                startLoop();
-              },
-            },
-          ]);
-          event.startDate = days;
-
-          events.current.push(event);
-        }
-      }
-    }, INTERVAL);
-  }
 
   return (
     <GameContext.Provider value={context}>{children}</GameContext.Provider>
