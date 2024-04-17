250c3ad49fd6e074a932b4d4b4956df4f9a0094f
han8129 <110092300+han8129@users.noreply.github.com>
2024-04-17 14:47:12 +0700

    document useInterval hook

--- hooks/useInterval.tsx
+++ hooks/useInterval.tsx
@@ -1,5 +1,14 @@
 import { useCallback, useEffect } from 'react';
 
+/**
+ * Continuously call the callback function between the given delay
+ *
+ * callback: The function to cache, this function will be update if any
+ * dependency changes
+ *
+ * dependencies: The list of reactive values are used within the callback
+ * function, changes of these values will update the callback function
+ */
 export default function useInterval(
     callback: Function,
     delay: number,
